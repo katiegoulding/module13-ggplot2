45d032a22c421a7aab96bb4624974fc944a875d0
Katie Goulding <katiegoulding@gmail.com>
2017-02-07 15:17:55 -0800

    Add in class particiaption

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,44 +2,101 @@
 
 # Install and load `ggplot2`
 # You will also want to load `dplyr`
+library(ggplot2)
+library(dplyr)
+##### In Class Excerise: plot the mpg data set, with highway milage on the x-axis and engine    02/07/2017
+##### displacement on the y-axis:
+
+ggplot(data = mpg) +
+    geom_point(mapping = aes(x = displ, y = hwy, color = class), size = 4) +
+    geom_smooth(mapping = aes(x = displ, y=hwy), se = FALSE)
+#  |
+#  V
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point()+
+  geom_smooth(se = FALSE)
+
+ggplot(data = mpg) +
+  geom_bar(mapping = aes(x = class))
+
+# ==
+  
+ggplot(data = mpg) +
+    stat_count(mapping = aes(x=class))
+
+ggplot(data = mpg) +
+    geom_bar(mapping = aes(x = class), stat = "count")
+
+ggplot(data = mpg) +
+    stat_count(mapping = aes(x = hwy, fill = class)) #Bar Charts Are FILL NOT COLOR. Color will only outline.
+
+ggplot(data = mpg) +
+    geom_point(mapping = aes(x = cty, y = hwy, color = class)) +
+      scale_x_reverse() +
+        scale_color_brewer(palette = "Set2") ##Or can use scale_color_hue(l= , c= )
+
+ggplot(data = mpg) +
+  geom_bar(mapping = aes(x = hwy, fill = class)) +
+    coord_flip()
+
+ggplot(mpg, aes(x = factor(1), fill = factor(cyl))) +
+    geom_bar(width = 1) +
+      coord_polar(theta = "y")
+
+ggplot (data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = class)) +
+    facet_wrap(~class + cyl, nrow = 5)  ## ~ represents its a formula, "as a function of"
 
 
 # For this exercise you'll be working with the `diamonds` data set included in the ggplot2 library
 # Use `?diamonds` to get more information about this data set (including the column descriptions
 # Also check the _column names_ and the _number of rows_ in the data set
-
+View(diamonds)
 
 # This data set has a lot of rows. To make things a bit more readable,
 # use dplyr's `sample_n()` function to get a random 1000 rows from the data set
 # Store this sample in a variable `diamonds.sample`
-
+diamonds.sample <- sample_n(diamonds, 1000)
 
 # Start by making a new `ggplot` with the `diamonds.sample` as the data (no geometry yet)
 # What do you see?
-
+ggplot(data = diamonds.sample)
 
 # Draw a scatter plot (with point geometry) with for the `diamonds.sample` set,
 # with the `carat` mapped to the x-position and `price` mapped to the y-position.
 
-
+ggplot(data = diamonds.sample, mapping = aes(x = carat, y = price)) +
+  geom_point()
 # Draw the same plot as above, but color each of the points based on their clarity.
 
+ggplot(data = diamonds.sample, mapping = aes(x = carat, y = price, color = clarity)) +
+  geom_point()
 
 # Draw the same plot as above, but for the entire `diamonds` data set. Note this may take
 # a few seconds to generate.
 
+ggplot(data = diamonds, mapping = aes(x = carat, y = price, color = clarity)) +
+  geom_point()
 
 # Draw another scatter plot for `diamonds.sample` of price (y) by carat (x),
 # but with all of the dots colored "blue".
 # Hint: you'll need to set the color channel, not map a value to it!
 
+ggplot(data = diamonds.sample, mapping = aes(y = price, x = carat)) +
+  geom_point(color = "blue")
 
 # Draw a scatter plot for `diamonds.sample` of `price` by `carat`, where each
 # point has an aesthetic _shape_ based on the diamond's `cut`.
 
+ggplot(data = diamonds.sample, mapping = aes(y = price, x = carat, shape = cut)) +
+    geom_point()
 
 # Draw a scatter plot for `diamonds.sample` of *`cut`* by `carat`, where each
 # point has an aesthetic _size_ based on the diamond's *`price`*
+
+ggplot(data = diamonds.sample, mapping = aes(y = cut, x = carat, size = price, color = price)) +
+  geom_point()
 
 
 # Try coloring the above plot based on the diamond's price!
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,7 +2,7 @@
 
 # Install and load `ggplot2`
 # install.packages("ggplot2") # if needed
-
+library(ggplot2)
 
 # For this exercise you will again be working with the `diamonds` data set.
 # Use `?diamonds` to review details about this data set
@@ -13,33 +13,42 @@
 
 # Draw a bar chart of the diamonds data, organized by cut
 # The height of each bar is based on the "count" (number) of diamonds with that cut
-
+ggplot(data = diamonds, mapping = aes(x = cut)) +
+  geom_bar()
 
 # Use the `stat_count` to apply the statistical transformation "count" to the diamonds
 # by cut. You do not need a separate geometry layer!
-
+ggplot(data = diamonds, mapping = aes(x = cut)) +
+  stat_count()
 
 # Use the `stat_summary` function to draw a chart with a summary layer.
 # Map the x-position to diamond `cut`, and the y-position to diamond `depth`
 # Bonus: use `min` as the function ymin, `max` as the function ymax, and `median` as the function y
 
-
+ggplot(data = diamonds, mapping = aes(x = cut, y = depth)) +
+  stat_summary(fun.ymin = min, fun.ymax = max, fun.y = median)
+##    ==
+ggplot(data = diamonds) +
+  stat_summary(mapping = aes(x = cut, y = depth), fun.ymin = min, fun.ymax = max, fun.y = median)
 
 ## Position Adjustments
 
 # Draw a bar chart of diamond data organized by cut, with each bar filled by clarity.
 # You should see a _stacked_ bar chart.
-
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x=cut, fill = clarity))
 
 # Draw the same chart again, but with each element positioned to "fill" the y axis
-
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x=cut, fill = clarity), position = "fill")
 
 # Draw the same chart again, but with each element positioned to "dodge" each other
 
 
 # Draw a plot with point geometry with the x-position mapped to `cut` and the y-position mapped to `clarity`
 # This creates a "grid" grouping the points
-
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = cut, y = clarity))
 
 # Use the "jitter" position adjustment to keep the points from all overlapping!
 # (This works a little better with a sample of diamond data, such as from the previous exercise).
